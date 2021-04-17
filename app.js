bddebdd0d76760f02f2c43f2e56390a0b4a8853c
ezekiel444 <ezekielmatomi588@gmail.com>
2021-04-18 00:24:02 +0200

    all cart functionlity ok

--- app.js
+++ app.js
@@ -43,8 +43,8 @@ class UI{
           <div class="img-container">
               <img src=${product.url} class="product-img" alt="product">
               <button class="bag-btn" data-id=${product.id}>
-                  <i class="fas fa-shoppig-cart"></i>
-                  add to bag
+              <i class="fas fa-shopping-cart"></i>
+                  add to cart
               </button>
           </div>
           <h3>${product.title}</h3>
@@ -118,6 +118,85 @@ setCartValues(cart){
 cartOverLay.classList.add('transparentBcg')
 cartDOM.classList.add('showCart')
     }
+
+    setupAPP(){
+cart = Storage.getCart()
+this.setCartValues(cart)
+this.populate(cart)
+cartBtn.addEventListener('click', this.showCart)
+closeCartBtn.addEventListener('click',this.hideCart)
+    }
+    populate(cart){
+        cart.forEach(item=>this.addCartItem(item))
+
+    }
+    hideCart(){
+        cartOverLay.classList.remove('transparentBcg')
+cartDOM.classList.remove('showCart')
+    }
+
+    cartLogic(){
+        // clear cart button
+        clearCartBtn.addEventListener('click', ()=>{
+            this.clearCart()
+        })
+        // cart functionality
+        cartContent.addEventListener('click',(event)=>{
+
+if(event.target.classList.contains('remove-item')){
+     let removeItem = event.target
+
+let id = removeItem.dataset.id
+cartContent.removeChild(removeItem.parentElement.parentElement)
+this.removeItem(id)
+}else if(event.target.classList.contains("fa-chevron-up")){
+let addAmount = event.target
+let id = addAmount.dataset.id
+let tempItem = cart.find(item=>item.id===id)
+tempItem.amount +=1
+Storage.saveCart(cart)
+this.setCartValues(cart)
+addAmount.nextElementSibling.innerText = tempItem.amount
+
+}else if(event.target.classList.contains("fa-chevron-down")){
+    let lowerAmount = event.target
+    let id = lowerAmount.dataset.id
+    let tempItem = cart.find(item=>item.id===id)
+tempItem.amount -=1
+if(tempItem.amount > 0){
+Storage.saveCart(cart)
+this.setCartValues(cart)
+lowerAmount.previousElementSibling.innerText = tempItem.amount
+}else{
+    cartContent.removeChild(lowerAmount.parentElement.parentElement)
+    this.removeItem(id)
+}
+}
+
+})
+    }
+
+    clearCart(){
+      let cartItems = cart.map(item=>item.id)
+
+cartItems.forEach(id=>this.removeItem(id)) 
+  while(cartContent.children.length > 0){
+      cartContent.removeChild(cartContent.children[0])
+  }
+  this.hideCart()
+ }
+ removeItem(id){
+     cart = cart.filter(item=>item.id !==id)
+     this.setCartValues(cart)
+     Storage.saveCart(cart)
+     let button = this.getSingleButton(id)
+     button.disabled = false
+     button.innerHTML = `<i class="fas fa-shopping-cart"></i>add to cart `
+
+ }
+ getSingleButton(id){
+     return buttonsDOM.find(button=>button.dataset.id===id)
+ }
 }
 
 
@@ -135,15 +214,24 @@ return products.find(product=>product.id === id)
     static saveCart(cart){
         localStorage.setItem('cart',JSON.stringify(cart))
     }
+    static getCart(){
+        return localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : []
+    }
 }
 
 document.addEventListener("DOMContentLoaded", ()=>{
     const ui = new UI();
     const products = new Products()
+
+// setup app
+ui.setupAPP()
+
+    // get all products
     products.getProducts().then(product=>{
         ui.displayProducts(product)
         Storage.saveProducts(product)
     }).then(()=>{
         ui.getProductsBtn()
+        ui.cartLogic()
     })
 })
